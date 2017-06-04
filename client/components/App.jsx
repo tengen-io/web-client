d2efb08fd5e11bcf42a378f2ae436736eab73877
Ian Wessen <ian@honeybook.com>
2017-06-04 01:47:28 -0700

    init webpack & react

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div style={{
+                padding: 200,
+                textAlign: center
+            }}><h1>Hello, world!</h1></div>
+        )
+    }
+}
+
+export default App;
