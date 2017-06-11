87b09a71700cb75b511140d8ff7ac2618576b1d5
Ian Wessen <ian@honeybook.com>
2017-06-10 17:39:27 -0700

    heavily commenting the config for future reference

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 
-
+// # Path
+// Duplicate of Node core path module
 const path = require('path');
 
+// # HtmlWebpackPlugin
+// Takes a template .html and makes a [filename].html which dynamically includes
+// other webpack bundles using <script> tags so you don't have to sweat changing
+// the <script> tag as bundles get created.
+//
+// ( Especially useful for fingerprinted bundles )
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// TODO: Setup JSON config dir
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './client/index.html',
     filename: 'index.html',
