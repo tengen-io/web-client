7b03781492d320012a8bba5a63c33cb1c5e30093
Ian Wessen <ian@honeybook.com>
2017-06-27 23:53:30 -0700

    color scheme and initial application

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,16 @@
 
 import React, { Component } from "react";
+import Header from "./partials/header";
 
 export default class App extends Component {
     
     render() {
 
         return (
-            <div>
+            <section>
+                <Header/>
                 { this.props.children }
-            </div>
+            </section>
         );
 
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,46 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  context: __dirname,
-  entry: "./src/index.jsx",
-  output: {
-    path: __dirname + '/public',
-    filename: "bundle.js",
-    publicPath: '/public/'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js|.jsx?$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('css-loader!sass-loader')
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new ExtractTextPlugin({ filename: 'app.css', allChunks: true })
-  ],
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+    
+    context: __dirname,
+    
+    entry: "./src/index.jsx",
+    
+    output: {
+        path: __dirname + '/public',
+        filename: "bundle.js",
+        publicPath: '/public/'
+    },
+    
+    module: {
+        loaders: [
+            {
+                test: /\.js|.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+            }
+        ]
+    },
+    
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    
+    plugins: [
+        new ExtractTextPlugin({ filename: 'app.css', allChunks: true })
+    ],
+    
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
 };
