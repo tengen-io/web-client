730ecff9fd5a957bc614836cc7760e39e6cf335d
Ian Wessen <ian@honeybook.com>
2018-01-27 21:28:32 -0800

    builds out board logic in pure JS

--- board.js
+++ board.js
@@ -58,8 +58,10 @@ Board.prototype.endGame = function endGame() {
 };
 
 
-
+// (coordinates) => boolean
 Board.prototype.play = function play(i,j) {
+
+    var _ = _;
 
     console.log('Played at ' + i + ', ' + j);
 
@@ -74,7 +76,7 @@ Board.prototype.play = function play(i,j) {
     var neighbors = this.getAdjacentIntersections(i,j);
     var atari = false;
 
-    var self = this;
+    let self = this;
     _.each(neighbors, function (n) {
         var state = self.board[n[0]][n[1]];
         if (state !== Board.EMPTY && state !== color) {
@@ -82,12 +84,94 @@ Board.prototype.play = function play(i,j) {
             console.log('group', group);
             if (group['liberties' === 0]) {
                 captured.push(group);
-            else if (group['liberties'] === 1) {
+            } else if (group['liberties'] === 1) {
                 atari = true;
             }
         }
     });
 
-    // if (_.isEmpty())
+    if (_.isEmpty(captured) && this.getGroup(i,j)['liberties'] === 0) {
+        this.board[i][j] = Board.EMPTY;
+        this.attemptedSuicide = true;
+        return false;
+    }
+
+    _.each( captured, function (group) {
+        _.each( group, function (stone) {
+            self.board[ stone[0] ][ stone[1] ] = Board.EMPTY;
+        });
+    });
+
+    if (atari) {
+        this.inAtari = true;
+    }
+
+    this.lastMovePassed = false;
+    this.switchPlayer();
+    return true;
+
+};
+
+
+
+Board.prototype.getAdjacentIntersections = function (i,j) {
+    var neighbors;
+    if (i > 0) {
+        neighbors.push([ (i - 1), j ]);
+    }
+    if (j < this.size - 1) {
+        neighbors.push([ i, (j + 1) ]);
+    }
+    if (i < this.size - 1) {
+        neighbors.push([ (i + 1), 1 ]);
+    }
+    if (j > 0) {
+        neighbors.push([ i, (j - 1) ]);
+    }
+    return neighbors;
+};
+
+
+
+Board.protoype.getGroup = function (i,j) {
+
+    var _ = _;
+
+    var color = this.board[i][j];
+    if (color === Board.EMPTY) {
+        return null;
+    }
+
+    var visited = {}; // for O(1) lookups
+    var visitedList = []; // for returning
+    var queue = [[i, j]];
+    var count = 0;
+
+    while (queue.length > 0) {
+        var stone = queue.pop();
+        if (visited[stone]) {
+            continue;
+        }
+
+        var neighbors = this.getAdjacentIntersections(stone[0], stone[1]);
+        var self = this;
+        _.each( neighbors, function (n) {
+            var state = self.board[n[0]][n[1]];
+            if (state === Board.EMPTY) {
+                count++;
+            }
+            if (state === color) {
+                queue.push(n[0],n[1]);
+            }
+        });
+
+        visited.stone = true;
+        visitedList.push(stone);
+    }
+
+    return {
+        'liberties': count,
+        'stones': visitedList
+    }
 
 };
