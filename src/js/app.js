d6cbf43f2c8f4f6916ddeaef3b1544042b58485d
Cam <cjirmas@gmail.com>
2018-02-19 18:51:59 -0800

    Add basic Board & Intersections

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,37 @@ import { render } from 'react-dom';
 
 require('../stylesheets/app.scss');
 
-export default class Hello extends Component {
-  render() {
-    return (
-      <p>Hello from react</p>
-    );
+const Board = (size) => {
+  let res = [];
+
+  for (var x = 0; x < size; x++) {
+    for (var y = 0; y < size; y++) {
+      // For now, random color value
+      res.push([x, y, Math.floor(Math.random() * Math.floor(3))])
+    }
   }
+
+  return (
+    <div className='board'>
+      { res.map(Intersection) }
+    </div>
+  )
 }
 
-render(<Hello />, document.getElementById('app'));
+const Intersection = ([x, y, color]) => {
+  let colorClass;
+  if (color === 0) {
+    colorClass = 'White';
+  } else if (color === 1) {
+    colorClass = 'Black';
+  } else {
+    colorClass = 'Brown';
+  }
+
+  return (
+    <div key={ `${x},${y}` } className={ `intersection${colorClass}` }>{ x }, { y }</div>
+  )
+}
+
+// Example for 9x9 grid
+render(Board(9), document.getElementById('app'));
