d62b3c5665107f686e92f12a49ea7818d1326dbd
Cam <cjirmas@gmail.com>
2018-04-08 02:46:29 -0700

    Add Apollo config

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+
 import { BOARD } from './utils/constants';
 import Game from './components/game';
 
 require('../stylesheets/app.scss');
 
+const client = new ApolloClient({
+  // TODO: replace with env config
+  uri: "http://localhost:4000/api"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        users {
+          username
+        }
+      }
+    `
+  })
+  .then(data => console.log({ data }));
+
 render(
-    <Game />, 
+    <ApolloProvider client={ client }><Game /></ApolloProvider>,
     document.getElementById('app'));
