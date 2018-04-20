8ede872a176bc32d40cc98be0ff531fce19b2feb
Ian Wessen <ian@honeybook.com>
2018-04-20 08:12:01 -0700

    wip

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { BOARD } from './utils/constants';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {BOARD} from './utils/constants';
 import Game from './components/game';
 
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
 const client = new ApolloClient({
-  //uri: 'https://go-stop.live/api',
-  uri: 'http://localhost:4000/api',
-  //uri: process.env['API_URL']
+    uri: 'https://go-stop.live/api',
+    // uri: 'http://localhost:4000/api',
+    //uri: process.env['API_URL']
 });
 
 require('../stylesheets/app.scss');
 
 render(
-    <ApolloProvider client={ client }><Game size={BOARD.SIZE} /></ApolloProvider>,
-    document.getElementById('app'));
+    <ApolloProvider client={client}>
+        <Game size={BOARD.SIZE} />
+    </ApolloProvider>,
+    document.getElementById('app')
+);
