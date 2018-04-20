2a4d2654ad79794248b1954206df7dccf8f42057
Cam <cjirmas@gmail.com>
2018-04-19 22:27:24 -0700

    Add game query to Game component

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,17 @@ import { render } from 'react-dom';
 import { BOARD } from './utils/constants';
 import Game from './components/game';
 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  //uri: 'https://go-stop.live/api',
+  uri: 'http://localhost:4000/api',
+  //uri: process.env['API_URL']
+});
+
 require('../stylesheets/app.scss');
 
 render(
-    <Game size={BOARD.SIZE}/>, 
+    <ApolloProvider client={ client }><Game size={BOARD.SIZE} /></ApolloProvider>,
     document.getElementById('app'));
