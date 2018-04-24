715e3a41af845e79a47c7a78db4060eb4db4801e
Ian Wessen <ianwessen@gmail.com>
2018-04-23 20:33:50 -0700

    set graphql apollo to live server

--- src/js/app.js
+++ src/js/app.js
@@ -7,8 +7,8 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-  //uri: 'https://go-stop.live/api',
-  uri: 'http://localhost:4000/api',
+  uri: 'https://go-stop.live/api',
+  // uri: 'http://localhost:4000/api',
   //uri: process.env['API_URL']
 });
 
